e8244402488a9fb623867acfe62dd735acb9a979
igor <k1785@rambler.ru>
2015-01-02 16:55:15 +0300

    'first_route_AND_controller'

---
+++ frontend/controllers/MainController.php
@@ -0,0 +1,24 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
+
+class MainController extends Controller
+{
+
+    public function actionSay($message = 'Привет')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
+}
---
+++ frontend/views/main/say.php
@@ -0,0 +1,7 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = 'Say '.$message;
+?>
+<div class="content">
+    <? echo $message ?>
+</div>
