f8b3417b5b5a8ada532db1b06ed2a7442aab1f48
igor <k1785@rambler.ru>
2015-01-20 21:27:51 +0300

    'add_category_model'

---
+++ frontend/models/Category.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $alias
+ * @property string $description
+ * @property integer $sort
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'alias', 'description', 'sort'], 'required'],
+            [['description'], 'string'],
+            [['sort'], 'integer'],
+            [['name', 'alias'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'alias' => 'Alias',
+            'description' => 'Description',
+            'sort' => 'Sort',
+        ];
+    }
+}
